1fd131c439620a5998a629e3b7c47bebba371774
RrChaos <rrchaos@tutanota.com>
2019-04-14 14:07:33 +0100

    Cartoons Episodes App

--- myappts/src/Store.tsx
+++ myappts/src/Store.tsx
@@ -1,14 +1,11 @@
-//https://api.tvmaze.com/singlesearch/shows?q=rick-&-morty&embed=episodes
 // react
-import React from 'react'
-import { func } from 'prop-types';
+import React, { ReactElement  ,  ReactComponentElement } from 'react'
 
 interface IState {
-  episodes : [] , 
+  episodes : []  
   favourites: []
 }
-//STORE SECTION , REDUX
-//ALL DATA COMING HERE
+//STORE SECTION , REDUX / ALL DATA COMING HERE
 
 //The initial state for Context-Obj for React.createContext
 const initialState : IState = {
@@ -17,18 +14,18 @@ const initialState : IState = {
 }
 
 //used by all components in my app { provider , }
-export const Store = React.createContext<IState>( initialState )
+export const Store = React.createContext<IState | any>( initialState )
 
 //-----------------------------------------------
 
 interface IAction {
-  type: string,
+  type: string
   payload: any
 }
 
 // REDUCER  >>  TO MODIFY STORE
 function reducer( state: IState, action : IAction) : IState {
-  switch (action.type) {
+  switch  ( action.type ) {
     case  'FETCH_DATA' : //get data from API
       return { ...state, episodes :  action.payload }   //populate Store with it
     default :
@@ -37,6 +34,7 @@ function reducer( state: IState, action : IAction) : IState {
 }
 
 //provide all components with access to Store
-export function StoreProvider(props : any) : JSX.Element {
-  return <Store.Provider value = { initialState }>{props.children}</Store.Provider>
-}
+export function StoreProvider( props : any ) : JSX.Element {
+  const [ state , dispatch ] = React.useReducer( reducer , initialState);
+  return <Store.Provider value={{state , dispatch }}>{props.children}</Store.Provider>
+} 
