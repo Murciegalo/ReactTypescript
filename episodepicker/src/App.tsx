cde5e97d8add235123214b00bd8204ecf8f4f4f6
RrChaos <rrchaos@tutanota.com>
2019-04-27 14:23:55 +0100

    WP , Babel , React , Typescript env. set

---
+++ episodepicker/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles/style.scss';
+//import logo from './logo.svg';
+//<img src={logo} className="App-logo" alt="logo" />
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+    </div>
+  );
+}
+
+export default App;
